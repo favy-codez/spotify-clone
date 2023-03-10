b976e031faf4ca3d0e9ecaea4e50f8de5578f761
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-03-10 12:34:40 +0100

    added components

---
+++ Desktop/spotify-clone/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar"
+import Table from "./components/Table"
+import Footer from "./components/Footer"
+
+export default App
