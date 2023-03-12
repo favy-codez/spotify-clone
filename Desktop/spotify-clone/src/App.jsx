952967004b1dbba7bec75013e907047dcd0674ac
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-03-12 16:48:56 +0100

    made changes to the components

--- Desktop/spotify-clone/src/App.jsx
+++ Desktop/spotify-clone/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import Header from "./components/RightSidebar";
 import Sidebar from "./components/Sidebar"
@@ -7,3 +7,26 @@ import Table from "./components/Main"
 import Footer from "./components/Login"
 
 export default App
+
+import './App.css';
+import React from'react';
+import Main from './components/Main.jsx'
+import LeftSidebar from './components/LeftSidebar.jsx'
+import RightSidebar from './components/RightSidebar.jsx'
+import 'remixicon/fonts/remixicon.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LeftSidebar />
+      <Main />
+      <RightSidebar />
+
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
