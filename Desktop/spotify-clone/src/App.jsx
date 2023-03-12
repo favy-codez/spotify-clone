08b81fe325ff110bac4b676be05bf791d24136f5
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-03-12 16:45:25 +0100

    made changes to the components

--- Desktop/spotify-clone/src/App.jsx
+++ Desktop/spotify-clone/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Header from "./components/Header";
+import Header from "./components/RightSidebar";
 import Sidebar from "./components/Sidebar"
-import Table from "./components/Table"
-import Footer from "./components/Footer"
+import Table from "./components/Main"
+import Footer from "./components/Login"
 
 export default App
